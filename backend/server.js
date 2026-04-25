2bad4a48e65ba435ae968357385aa491d6da388f
Adipatil1214 <p.aditya1412@gmail.com>
2026-04-25 19:37:29 +0530

    Deployment changes

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://expense-splitter-v2-receipts-manage.vercel.app/",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
